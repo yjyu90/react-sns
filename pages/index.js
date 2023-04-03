71fbc26865a890bd004588b29e6820b05db7341d
유영진 <youngjin11@nsonesoft.com>
2023-04-03 19:07:27 +0900

    Revert "PostCard 개발"
    This reverts commit 4b11dc9cfcbebe557d4a4f1dd924ac4560f7d8b3.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import AppLayout from "../components/AppLayout";
 import {useSelector} from "react-redux";
-import PostForm from "../components/PostForm";
-import PostCard from "../components/PostCard";
 const Home = () =>{
     const {isLoggedIn} = useSelector(state => state.user);
     const {mainPosts} = useSelector(state => state.post);
