4e9a750578dd86cd10e7023c4c9525bad3cf897d
yjyu90 <zzyoungjin@naver.com>
2023-04-06 01:13:54 +0900

    PostImages.js 개발 중

--- components/PostImages.js
+++ components/PostImages.js
@@ -1,6 +1,19 @@
 import PropTypes from "prop-types";
+import {useCallback, useState} from "react";
 
 const PostImages = ({images}) => {
+    const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+    const onZoom = useCallback((e) => {
+        setShowImagesZoom(true);
+    },[]);
+    if(images.length === 1){
+        return (
+            <>
+                <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+            </>
+        )
+    }
     return (
         <div></div>
     )
