d48ad70d4def823083cf62315550596b0aa40596
yjyu90 <zzyoungjin@naver.com>
2023-04-07 00:57:21 +0900

    ImageZoom 컴포넌트 개발

---
+++ components/ImagesZoom/index.js
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import Slick from 'react-slick';
+import {useState} from "react";
+const ImagesZoom = ({images, onClose}) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    return (
+        <div>
+            <header>
+                <h1>상세 이미지</h1>
+                <button onClick={onClose}>X</button>
+            </header>
+            <div>
+                <div>
+                    <Slick
+                        initialSlide={0}
+                        afterChange={(slide) => setCurrentSlide(slide)}
+                        infinite
+                        arrows={false}
+                        slidesToShow={1}
+                        slidesToScroll={1}
+                    >
+                        {images.map((v) => (
+                          <div key={v.src}>
+                              <img src={v.src} alt={v.src} />
+                          </div>
+                        ))}
+                    </Slick>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ImagesZoom.propTypes = {
+    images : PropTypes.arrayOf(PropTypes.object).isRequired,
+    onClose : PropTypes.func.isRequired,
+};
+
+export default ImagesZoom;
--- components/PostImages.js
+++ components/PostImages.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import {useCallback, useState} from "react";
+import {PlusOutlined} from "@ant-design/icons";
 
 const PostImages = ({images}) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -7,15 +8,45 @@ const PostImages = ({images}) => {
     const onZoom = useCallback((e) => {
         setShowImagesZoom(true);
     },[]);
+
+    const onClose = useCallback((e) => {
+        setShowImagesZoom(false);
+    },[]);
+
     if(images.length === 1){
         return (
             <>
-                <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                <img role="presentation"  src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom image={images} onClose={onClose} />}
+            </>
+        )
+    }
+    if(images.length === 2){
+        return (
+            <>
+                <img role="presentation" style={{width : "50%", display: 'inline-block'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                <img role="presentation" style={{width : "50%", display: 'inline-block'}} src={images[1].src} alt={images[1].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom image={images} onClose={onClose} />}
             </>
         )
     }
     return (
-        <div></div>
+        <>
+            <div>
+                <img role="presentation" style={{width : "50%"}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                <div
+                    role="presentation"
+                    style={{display: 'inline-block', width:'50%', textAlign:'center', verticalAlign:'middle'}}
+                    onClick={onZoom}
+                    >
+                    <PlusOutlined />
+                    <br />
+                    {images.length - 1}
+                    개의 사진 더보기
+                </div>
+            </div>
+            {showImagesZoom && <ImagesZoom image={images} onClose={onClose} />}
+        </>
     )
 }
 
