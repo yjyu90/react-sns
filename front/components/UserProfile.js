94b25098d0fb787f8513a621e9dbcbaa4fe8d291
yjyu90 <zzyoungjin@naver.com>
2023-04-22 16:33:30 +0900

    frontend, backend 분리

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -16,14 +16,14 @@ const UserProfile = () =>{
     return(
         <Card
             actions={[
-                <div key="twit">짹짹<br />{me.Posts.length}</div>,
-                <div key="followings">팔로잉<br />{me.Followings.length}</div>,
-                <div key="follower">팔로워<br />{me.Followers.length}</div>,
+                <div key="twit">짹짹<br />{me?.Posts?.length}</div>,
+                <div key="followings">팔로잉<br />{me?.Followings?.length}</div>,
+                <div key="follower">팔로워<br />{me?.Followers?.length}</div>,
             ]}
         >
             <Card.Meta
-                avatar={<Avatar>{me.nickname[0]}</Avatar>}
-                title={me.nickname}
+                avatar={<Avatar>{me?.nickname ? me?.nickname[0] : ''}</Avatar>}
+                title={me?.nickname}
             />
             <Button onClick={onLogOut} loading={logOutLoading}>로그아웃</Button>
         </Card>
